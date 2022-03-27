b5a6b70181bd406b933a5f66b4b94072b4ed02b5
jakebolger <c18395341@mytudublin.ie>
2022-03-27 17:13:26 +0100

    change

--- Wild-Ireland/Assets/Scripts/SceneSequence.cs
+++ Wild-Ireland/Assets/Scripts/SceneSequence.cs
@@ -9,9 +9,10 @@ public class SceneSequence : MonoBehaviour
     public GameObject Cam2;
     public GameObject UI;
     public GameObject Symbol;
-    //public GameObject Cam3;
-    //public GameObject Cam4;
-    //public GameObject Cam5;
+    public GameObject Cam3;
+    public GameObject Cam4;
+    public GameObject Cam5;
+    public GameObject Title;
     //public GameObject Cam6;
     //public GameObject Cam7;
     //public GameObject Cam8;
@@ -25,18 +26,22 @@ public class SceneSequence : MonoBehaviour
     }
     IEnumerator TheSequence()
     {
-        yield return new WaitForSeconds(50);
+        yield return new WaitForSeconds(11);
         
         Cam2.SetActive(true);
         Cam1.SetActive(false);
-        //yield return new WaitForSeconds(4);
-        //Cam3.SetActive(true);
-        //Cam2.SetActive(false);
-        //yield return new WaitForSeconds(4);
-        //Cam4.SetActive(true);
-        //Cam3.SetActive(false);
-        //yield return new WaitForSeconds(4);
-        //Cam5.SetActive(true);
+        yield return new WaitForSeconds(5);
+        Cam3.SetActive(true);
+        Cam2.SetActive(false);
+        yield return new WaitForSeconds(5);
+        Cam5.SetActive(true);
+        Cam3.SetActive(false);
+        yield return new WaitForSeconds(8);
+        Cam4.SetActive(true);
+        Cam5.SetActive(false);
+        yield return new WaitForSeconds(8);
+        Cam1.SetActive(true);
+        Cam4.SetActive(false);
         //Cam4.SetActive(false);
         //yield return new WaitForSeconds(4);
         //Cam6.SetActive(true);
@@ -50,12 +55,13 @@ public class SceneSequence : MonoBehaviour
         //yield return new WaitForSeconds(4);
         //Cam9.SetActive(true);
         //Cam8.SetActive(false);
-        
-        yield return new WaitForSeconds(4);
-        Cam2.SetActive(false);
+
+        yield return new WaitForSeconds(15);
+        Cam1.SetActive(false);
         player.SetActive(true);
         UI.SetActive(true);
         Symbol.SetActive(true);
+        Title.SetActive(false);
         
     }
 
