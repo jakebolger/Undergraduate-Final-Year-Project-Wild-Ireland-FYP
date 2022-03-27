53e85e9ee6ca9204a0e45bb162445b9e49a81c31
jakebolger <c18395341@mytudublin.ie>
2022-03-27 20:37:26 +0100

    minimapas

---
+++ Wild-Ireland/Assets/minimapo.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class minimapo : MonoBehaviour
+{
+
+    public Transform player;
+    // Start is called before the first frame update
+    void LateUpdate()
+    {
+        Vector3 newPosition = player.position;
+        newPosition.y = transform.position.y;
+        transform.position = newPosition;
+
+        transform.rotation = Quaternion.Euler(90f, player.eulerAngles.y, 0f);
+    }
+}
